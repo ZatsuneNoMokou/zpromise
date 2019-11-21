868ea14544035e96c8d234f8630f5ef1255fd38b
Zatsune No Mokou <contact@zatsunenomokou.eu>
2019-11-21 14:17:18 +0100

    tests

---
+++ test/ZPromise.js
@@ -0,0 +1,33 @@
+const chai = require('chai'),
+	chaiAsPromised = require("chai-as-promised"),
+	{ZPromise} = require('../src/ZPromise')
+;
+
+const assert = chai.assert;
+chai.use(chaiAsPromised);
+
+describe('ZPromise.resolve', function () {
+	it('should resolve', function () {
+		const zPromise = new ZPromise(resolve => {
+			setTimeout(() => {
+				resolve(null)
+			}, 20000);
+		});
+		zPromise.resolve(42);
+
+		return assert.eventually.strictEqual(zPromise, 42);
+	});
+});
+
+describe('ZPromise.reject', function () {
+	it('should reject', function () {
+		const zPromise = new ZPromise(resolve => {
+			setTimeout(() => {
+				resolve(null)
+			}, 20000);
+		});
+		zPromise.reject('42');
+
+		return assert.isRejected(zPromise, /^42$/);
+	});
+});
--- test/allSettled.js
+++ test/allSettled.js
@@ -1,4 +1,10 @@
-const expect = require('expect.js');
+const chai = require("chai"),
+	chaiAsPromised = require("chai-as-promised")
+;
+
+const { expect, assert } = chai;
+chai.use(chaiAsPromised);
+
 const {ZPromise} = require('../src/ZPromise');
 
 var yes = function makeFulfilledResult(value) {
@@ -13,49 +19,49 @@ describe('ZPromise.allSettled', function () {
 	var b = {};
 	var c = {};
 
-	it('should have a ZPromise.allSettled', function () {
-		expect(typeof ZPromise.allsettled).to.be('function')
+	it('should be a function', function () {
+		assert.strictEqual(typeof ZPromise.allsettled, 'function')
 	});
 
 	it('all fulfilled', function () {
-		ZPromise.allSettled([
-			ZPromise.resolve(a),
-			ZPromise.resolve(b),
-			ZPromise.resolve(c)
-		]).then(function (results) {
-			expect(results).to.be([
-				yes(a),
-				yes(b),
-				yes(c)
-			]);
-		});
+		return expect(
+			ZPromise.allSettled([
+				ZPromise.resolve(a),
+				ZPromise.resolve(b),
+				ZPromise.resolve(c)
+			])
+		).to.eventually.deep.equal([
+			yes(a),
+			yes(b),
+			yes(c)
+		]);
 	});
 
 	it('all rejected', function () {
-		ZPromise.allSettled([
-			ZPromise.reject(a),
-			ZPromise.reject(b),
-			ZPromise.reject(c)
-		]).then(function (results) {
-			expect(results).to.be([
-				no(a),
-				no(b),
-				no(c)
-			]);
-		});
+		return expect(
+			ZPromise.allSettled([
+				ZPromise.reject(a),
+				ZPromise.reject(b),
+				ZPromise.reject(c)
+			])
+		).to.eventually.deep.equal([
+			no(a),
+			no(b),
+			no(c)
+		]);
 	});
 
 	it('mixed', function () {
-		ZPromise.allSettled([
-			a,
-			ZPromise.resolve(b),
-			ZPromise.reject(c)
-		]).then(function (results) {
-			expect(results).toBe([
-				yes(a),
-				yes(b),
-				no(c)
-			]);
-		});
+		return expect(
+			ZPromise.allSettled([
+				a,
+				ZPromise.resolve(b),
+				ZPromise.reject(c)
+			])
+		).to.eventually.deep.equal([
+			yes(a),
+			yes(b),
+			no(c)
+		]);
 	});
 });
